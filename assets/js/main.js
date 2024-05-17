92bc8f83e02ab746045b2e661afa4d7bb342d0f7
ken-warren <kennedykyalo168@gmail.com>
2024-05-17 20:25:52 +0300

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,14 @@ const sendEmail = (e) =>{
         .then(() =>{
             //show message that's been sent
             contactMessage.textContent = 'Message Sent Successfully'
+
+            //Set message timeout after 5secs
+            setTimeout(() =>{
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //clear input fields
+            contactForm.reset()
         }, () =>{
             //show error message
             contactMessage.textContent = 'Message not sent (service error)'
@@ -59,28 +67,6 @@ const sendEmail = (e) =>{
 }
 
 contactForm.addEventListener('submit', sendEmail)
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
-
-  sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }                                                    
-    })
-}
-window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
@@ -95,3 +81,11 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollup = () =>{
+    const scrollup = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll')
+                                            : scrollup.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollup)

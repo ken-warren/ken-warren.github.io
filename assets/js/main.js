27631ecd63b933e1ea462f496d5722e895ea0c40
ken-warren <kennedykyalo168@gmail.com>
2024-05-20 05:35:45 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,15 +73,16 @@ contactForm.addEventListener('submit', sendEmail)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
+    duration: 2500,
     delay: 200,
 //     reset: true
-});
+})
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__data, .home__social, .contact__container, .footer__container',{}); 
+sr.reveal('.home__image',{origin: 'bottom'}); 
+sr.reveal('.about__data, .skills__data',{ origin: 'left'}); 
+sr.reveal('.services__card, .projects__card',{interval: 100}); 
+sr.reveal('.about__image, .skills__content',{ origin: 'right'}); 
 
 /*=============== SHOW SCROLL UP ===============*/
 const scrollup = () =>{

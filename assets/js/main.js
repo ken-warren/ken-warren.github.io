7fa097e323110e026eb9b64901aca15bafaab095
ken-warren <kennedykyalo168@gmail.com>
2024-05-20 17:08:14 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,8 +81,8 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .home__social, .contact__container, .footer__container',{}); 
 sr.reveal('.home__image',{origin: 'bottom'}); 
 sr.reveal('.about__data, .skills__data',{ origin: 'left'}); 
-sr.reveal('.services__card, .projects__card',{interval: 100}); 
-sr.reveal('.about__image, .skills__content',{ origin: 'right'}); 
+sr.reveal('.services__card, .projects__card, .cert__img',{interval: 100}); 
+sr.reveal('.about__image, .skills__content, .cert__container',{ origin: 'right'}); 
 
 /*=============== SHOW SCROLL UP ===============*/
 const scrollup = () =>{
